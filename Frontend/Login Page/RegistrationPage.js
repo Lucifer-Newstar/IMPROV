f69396b3f4de3abc271b1935982f3df80a45fb72
harisharumugam94 <priyaharish055@gmail.com>
2026-06-22 20:28:28 +0530

    i can the URL

--- Frontend/Login Page/RegistrationPage.js
+++ Frontend/Login Page/RegistrationPage.js
@@ -11,7 +11,7 @@ const userdata={
     password: document.getElementById("password").value
 }
 
-fetch("http://localhost:8080/api/Users/register",{
+fetch("http://localhost:8080/api/users/register",{
     method: "POST",
     headers: {
         "Content-Type": "application/json"
